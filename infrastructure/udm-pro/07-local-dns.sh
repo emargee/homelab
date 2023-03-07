501abdb623bea4df31154fc5f6efc2c11c28b2f7
emargee <221695+emargee@users.noreply.github.com>
2023-03-07 21:13:35 +0000

    feat(sabnzbd,sonarr): Add services

--- infrastructure/udm-pro/07-local-dns.sh
+++ infrastructure/udm-pro/07-local-dns.sh
@@ -12,8 +12,20 @@ cat > /run/dnsmasq.conf.d/local_custom_dns.conf <<- "EOF"
 # Created by a UDM-Utilities run script
 # Change the domains and IP address to your own
 address=/onepiece.home.local/192.168.0.10
+address=/echoserver.home.local/192.168.2.10
+address=/hubble.home.local/192.168.2.10
+address=/rook.home.local/192.168.2.10
+address=/plex.home.local/192.168.2.20
+address=/goldilocks.home.local/192.168.2.10
+address=/weave.home.local/192.168.2.10
+address=/dashboard.home.local/192.168.2.10
+address=/minio.home.local/192.168.2.10
+address=/minio-console.home.local/192.168.2.10
+address=/sonarr.home.local/192.168.2.10
+address=/sabnzbd.home.local/192.168.2.10
 EOF
- 
+
 # Restart dnsmasq so it sees the new conf file
 pkill dnsmasq
+
 
